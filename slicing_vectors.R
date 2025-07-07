1d5854f98424e9ecd60a02b19cb721c7f20293eb
rosa-lpz <rosali.lopezen@outlook.com>
2025-07-07 07:58:35 -0700

    slicing_vectors.R

---
+++ slicing_vectors.R
@@ -0,0 +1,8 @@
+
+# Create a vector
+my_vector <- c(10, 20, 30, 40, 50)
+
+# Slicing: Extract elements from position 2 to 4
+sliced_vector <- my_vector[2:4]
+
+print(sliced_vector)  # Output: 20 30 40
